7487e9aea6e153a9dd51bc7150bf2cec4ff043d4
Himanshu Kumawat <135202238+iamhimanshu98@users.noreply.github.com>
2024-08-01 10:33:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == "__main__" :
+    app.run(debug=True)
